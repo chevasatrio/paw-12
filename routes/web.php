2ebb8aa7b50a49f562b879f0c9849febce434b72
cheva <chevautomo@gmail.com>
2025-12-31 01:05:42 +0700

    push baru

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/management-products', function () {
-        return view('components.management-products');
-    })->name('management-products');
-
 });
 
-route::middleware(['admin'])->group(function () {
-    Route::get('/management-products', function () {
-        return view('components.management-products');
-    })->name('management-products');
-});
-
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
